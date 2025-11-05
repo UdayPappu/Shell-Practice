1522058b522bafc64150e53b9fb9fdb4fd1de0c9
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-06 00:39:36 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2; Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1; I am fine too; Whats up?"
+echo "$PERSON2:: Nothing, Just going to mars will you come?"
