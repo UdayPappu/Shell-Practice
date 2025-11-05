08e1c863826f8df46ff471a760532a651b1e4b63
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-06 00:33:55 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "trump: Hey Musk, How are you?"
+echo "Musk; Hello Trump, I am fine. How are you?"
+echo "Trump; I am fine too; Whats up?"
+echo "Musk:: Nothing, Just going to mars will you come?"
