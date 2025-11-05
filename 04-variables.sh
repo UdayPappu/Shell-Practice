d715b3a367892e7d5e30cefb211c0423c6826781
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-06 00:47:25 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2; Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1; I am fine too; Whats up?"
+echo "$PERSON2:: Nothing, Just going to mars will you come?"
