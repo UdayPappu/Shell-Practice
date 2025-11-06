9803b9e238e7b925950b9c48242571f78c871016
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-06 13:11:21 +0530

    Special vars

---
+++ 11-functions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+echo "ERROR: Please run this script with root access"
+exit 1
+else 
+echo "you are running with root accesss"
+fi
+
+VALIDATE (){
+if [ $1 -eq 0 ]
+then 
+echo "Installing $2 is ... Success"
+else
+echo "Installation $2 ... Failed"
+exit 1
+fi 
+}
+
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+echo "MYSQL is not installed... going to install"
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+else
+echo "MYSQL is already installed..."
+fi 
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+echo "python3 is not installed... going to install"
+dnf install python3 -y
+VALIDATE $? "Python3"
+else
+echo "python3 is already installed..."
+fi 
+
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+echo "nginx is not installed... going to install"
+dnf install nginx -y
+VALIDATE $? "nginx"
+else
+echo "nginx is already installed..."
+fi 
+
