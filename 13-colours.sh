1413a2ab2aa7bfb17f6209822b91858254b2cc0e
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-06 18:23:56 +0530

    Colours

--- 13-colours.sh
+++ 13-colours.sh
@@ -14,7 +14,7 @@ then
 echo -e "$R ERROR: Please run this script with root access $N"
 exit 1
 else 
-echo "$G you are running with root accesss $N"
+echo -e "$G you are running with root accesss $N"
 fi
 
 VALIDATE (){
