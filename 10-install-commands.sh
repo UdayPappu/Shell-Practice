27e5c84060d46d8fcb900b4e2afb3086e7c9a573
Uday Pappu <saiudaybhaskarpappu@gmail.com>
2025-11-06 12:33:26 +0530

    Special vars

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -10,13 +10,23 @@ else
 echo "you are running with root accesss"
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0]
+then 
+echo "MYSQL is not installed... going to install"
 dnf install mysql -y
 
 if [ $? -eq 0 ]
 then 
-echo "Installing MQSQL is ... success"
+echo "Installing MQSQL is ... Success"
 else
-echo "Installation failed"
+echo "Installation ... Failed"
 exit 1
 fi 
+
+else
+echo "MYSQL is already installed..."
+fi 
+
 
